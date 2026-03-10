96535b8d30c1c510403340d85b7082810d79e405
am1nuos1 <chaoxiang0930@gmail.com>
2026-03-10 16:00:21 -0700

    gui

---
+++ app.py
@@ -0,0 +1,41 @@
+import time
+
+import streamlit as st
+from agent.react_agent import ReactAgent
+
+# Title
+st.title("Intelligent Customer Service Robot")
+st.divider()
+
+if "agent" not in st.session_state:
+    st.session_state["agent"] = ReactAgent()
+
+if "message" not in st.session_state:
+    st.session_state["message"] = []
+
+for message in st.session_state["message"]:
+    st.chat_message(message["role"]).write(message["content"])
+
+# User input prompt
+prompt = st.chat_input()
+
+if prompt:
+    st.chat_message("user").write(prompt)
+    st.session_state["message"].append({"role": "user", "content": prompt})
+
+    response_messages = []
+    with st.spinner("agent is thinking..."):
+        res_stream = st.session_state["agent"].execute_stream(prompt)
+
+        def capture(generator, cache_list):
+
+            for chunk in generator:
+                cache_list.append(chunk)
+
+                for char in chunk:
+                    time.sleep(0.01)
+                    yield char
+
+        st.chat_message("assistant").write_stream(capture(res_stream, response_messages))
+        st.session_state["message"].append({"role": "assistant", "content": response_messages[-1]})
+        st.rerun()
